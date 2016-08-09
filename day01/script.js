b1f99ef352b9a14189ae22a8f5261d4ae30aa83e
johnm0nty <montyoverzero@gmail.com>
2016-08-08 20:59:43 -0700

    first commit

---
+++ day01/script.js
@@ -0,0 +1,18 @@
+// alert('Hello World!');
+var name = 'John';
+var a = 5;
+var b = 6;
+var c = a + b;
+var e = true;
+var f = false;
+var g;
+var nothing = null;
+var something = undefined;
+var arr = ['John',25,false];
+var car = {
+  make:'Ford',
+  model:'Taurus',
+  year:2000
+};
+
+console.log(car.make);
